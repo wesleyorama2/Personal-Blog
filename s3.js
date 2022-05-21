29b888680625046c11f9dbe4390de836709998b3
wesleyorama2 <23392516+wesleyorama2@users.noreply.github.com>
2022-05-20 22:43:43 -0500

    Update s3.js

--- s3.js
+++ s3.js
@@ -1,6 +1,9 @@
 const s3 = require("s3-cached")({
   bucket: "personal-blog-mardowns",
-  s3Options: {},
+  s3Options: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID_BLOG,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_BLOG
+  },
   ttl: 43200,
 });
 
